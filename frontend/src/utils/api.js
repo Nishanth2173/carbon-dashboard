e17367e7be1c695f40fba89d5d697a46936900bb
Nishanth Nuthi <nishanthnuthi123@gmail.com>
2026-05-25 16:25:09 +0530

    base url updated

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://carbon-dashboard-backend.onrender.com',
+  baseURL: 'https://carbon-dashboard-backend.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
