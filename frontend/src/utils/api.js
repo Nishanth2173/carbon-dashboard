2a41517032dad9ea1e26e6e5ebba184fe2603118
Nishanth Nuthi <nishanthnuthi123@gmail.com>
2026-05-25 16:03:18 +0530

    Base url updated

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://carbon-dashboard-wine.vercel.app/',
+  baseURL: 'https://carbon-dashboard-backend.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 })
 
